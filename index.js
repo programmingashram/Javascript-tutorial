75e0b5c9f1511548cae711b6b6b05fb0b56ffbd4
Ayuhsman <avenuedesign768@gmail.com>
2022-12-31 04:22:37 +0530

    added chnages

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("helo");
+// Varoiables 
+let x = 5;
+let y = 8;
+
+// If - it means we can set an condition in if statment.
+if(x > y){
+    // if "x" is greater then "y", in case this statment is return this . 
+    console.log("X is greater then Y");
+}
