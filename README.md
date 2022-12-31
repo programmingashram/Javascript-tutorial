8a55ea473d19ef1e8998f0a71418faeb8aa2ab21
Ayuhsman <avenuedesign768@gmail.com>
2022-12-31 06:49:19 +0530

    added changes

---
+++ README.md
@@ -0,0 +1,13 @@
+## What is Turnery Operators  ?
+   Ternery operatores is short hand type of "if else" satatment. Where we write three lines code for if else , in ternery operator i'll code only one line.
+
+
+## what formate of ternery Operators ?
+    For Example - :
+
+    > (var1 > var2) ? "if condtion is true" : "if Condtion is false";
+
+    1. Here (x > y) =  Conditions.
+    2. ? "X is greater Y" = if my condtion is true then this 3 . 3. output will return.
+    3. :  "Statement is Incorrect !!!" = if my condtion is false then this output will return.
+
--- index.js
+++ index.js
@@ -1,118 +1,22 @@
-// DOM Document Object Model
-// in this DOM we can access, create, add, remove, Replace our HTML Element from Javascript
+// What is Turnery Operators  ?
+// -- Ternery operatores is short hand type of "if else" satatment.
+// -- where we write three lines code for if else , in ternery operatores 
+//--  we can write single line of code.
 
-// How can we access HTML Element In JS ??
-// _______________________________________________________
-// We can access HTML elements by mulitple ways for example
+//  For Example - :
 
-    // 1. documen.gteElementById(),
+//  Let me create some variables 
+let x = 40;
+let y = 20;
 
-        let x; // Declear a var
-        x = document.getElementById('idName'); //assign value in div var.
+//  know i'm creating one more variable for my ternery Operator.
 
-        // call variable in console
-        // console.log(x);
+let z = (x > y) ? "X is greater Y" : "Statement is Incorrect !!!";
 
-    // 2. document.getElementsByClassName();
-        let y; // Declear a var
-        y = document.getElementsByClassName('para'); //assign value in div var.
-
-        // in case i want second class elements it will 
-       y[1].style.color = "red";
-
-        // call variable in console
-        // console.log(y);
-
-    // 3. document.getElementsByTagName();
-
-        let z; // Declear a var
-        z = document.getElementsByTagName('p'); //assign value in div var.
-
-        // in case i want second class elements it will 
-        // z[3].style.color = "green";
-
-        // call variable in console
-        // console.log(z);
-        
-    // 4. document.querySelector();
-
-        let a; // Declear a var
-        a = document.querySelector('#idName'); //assign value in  var.
-
-        // QuerySelector is giving me firts indxing element
-
-        // call variable in console
-        console.log(a);
-
-    // 5. document.querySelectorAll();
-        let b; // Declear a var
-        b = document.querySelectorAll('.para')[2]; //assign value in  var.
-        // QuerySelector is giving me firts indxing element
-        b.style.fontSize = "2rem";
-        // call variable in console
-        console.log(b);
+//  Here (x > y) =  Conditions.
+//  ? "X is greater Y" = if my condtion is true then this output will return.
+//  : "Statement is Incorrect !!!" = if my condtion is false then this output will return.
 
 
-
-// How can we add or create  HTML Element from JS ??
-// _______________________________________________________
-
-//  We can add or create html element from javascript by using DOM
-
-    // -----[Create Element]---------
-
-    // 1. document.createElement()
-
-    // For Example - :
-        // let me access one parent element from html.
-        let body = document.getElementById('body'); 
-
-        //  lets create one element
-        // We are creating header tag
-        let header = document.createElement('header');
-       
-
-        // just append the newly created header tag in body
-
-        // Lets append Header in to body by variables
-        body.appendChild(header);
-
-    // -----[Add Element]---------
-    // 2. I have already craete an header variable, i'm just going to re use for addign element and text in under header tag
-
-        // For Example - :
-            //  Creating heading tag 
-            let headingTag = document.createElement('h1');
-
-            // add value in my headung
-            headingTag.innerHTML = "Hii I am Heading!!";
-
-            // Append  headign in header tag
-            header.appendChild(headingTag);
-           
-
-
-// How can we remove html elements from javascript ??
-// _______________________________________________________
-
-    // We can remove html Elements by using DOM method - 
-    //  We are removing or deleting html element by this method
-
-    // header.removeChild(headingTag);
-
-
-// How can we replace html elements from javascript ??
-// _______________________________________________________
-
-    // We can rplace html Elements by using DOM method - 
-    // i am using DOM method - 
-    
-    let hfour = document.getElementsByTagName('h4')[0];
-    //  We can replace our existing h4 tag to newly created h1 tag.
-    body.replaceChild(headingTag, hfour);
-
-
-
-        
-    
-
+//  let me complie the code.
+console.log(z);
