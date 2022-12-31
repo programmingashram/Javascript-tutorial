fa4b4e2b3dbd99469b809de4f5d5550c7b587570
Ayuhsman <avenuedesign768@gmail.com>
2022-12-31 06:26:52 +0530

    added changes

---
+++ README.md
@@ -0,0 +1,14 @@
+## What is if() and else if() Statement ?    
+    In this statment we can provide dif condition based on our code structure if() condition is matched we can give an return values in console and any where.
+
+## How can we write if else statement ?
+   For Example - :
+<code>
+    if(x > y){
+        console.log("X is greater then Y");
+    }
+</code>
+
+For Batter Understaing you can check our practicle example.
+
+
--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 // Varoiables 
 let x = 5;
 let y = 8;
-
+let output = "X is greater then Y";
 // If - it means we can set an condition in if statment.
 if(x > y){
     // if "x" is greater then "y", in case this statment is return this . 
     console.log("X is greater then Y");
 }
+
+//  Other wise , we have some default return
+// so in case we can write the code like = :
+
+
+if(x > y){
+    console.log(this.output);
+}
+else{
+    console.log("Y is less then x");
+}
+
+
+// Other wise , we have two conditions like 
+    // x is less then y and x is equal to y
+// so in case we can write the code like = :
+
+
+// you can changes the values of variable a and b and see the changes in outputs.
+
+
+let a = 20;
+let b = 20;
+
+if(a > b){
+    console.log("a is greater then b");
+}
+else if (a == b){
+    console.log('x is equal to Y');
+}
+else{
+    console.log("Statement is incorrect!!!");
+}
+
+
+
+
