d7ef5f2cba7afb2f4a72938e1e71460349a5529f
programmingashram <avenuedesign7658@gmail.com>
2023-01-15 09:39:52 +0530

    added

---
+++ index.js
@@ -0,0 +1,69 @@
+//  Var Keyword
+
+// Decleariation
+var x;
+
+// Assigning
+x = 10;
+
+// Outputs
+console.log(x); // it will return = 10
+
+// Let's re declear and re assign values in var scope
+
+
+// Re Decleariation
+var x;
+
+// Re Assigning
+x = 40;
+
+// Outputs
+console.log(x); // it will return = 40
+
+// ------------------------------------------------------------------
+
+//  let Keyword
+
+// Decleariation
+let y;
+
+// Assigning
+y = 100;
+
+// Outputs
+console.log(y); // it will return = 100
+
+// Let's re declear and re assign values in var scope
+
+// Re Decleariation
+// let y; // That'll returen error like - y is already decleard.
+
+// Re Assigning
+y = 80;
+
+// Outputs
+console.log(y); // it will return = 80
+
+
+// Outputs
+console.log(x); // it will return = 40
+
+// ------------------------------------------------------------------
+
+//  const Keyword
+
+// Decleariation
+
+// Do not use previous formate like
+
+// Decleariation
+// let y;
+
+// // Assigning
+// y = 100;
+// --- It will return error.
+
+// We can not re declear and re assign multiple values in const
+const a = 90;
+
