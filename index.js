da0be7da201f29557a81e371b10f1b8207e05382
programmingashram <avenuedesign7658@gmail.com>
2023-01-18 15:44:47 +0530

    added files

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function test(){
+    let el = document.querySelector('.content');
+    el.style.backgroundColor = "red";
+    el.style.color = "white";
+    el.style.padding = "10px";
+}
+
+
+function test2(){
+    let element = document.getElementById('input');
+    console.log(element.value);
+}
